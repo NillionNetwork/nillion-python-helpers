5c81e70df0ebbe6efb2a6dfe382ace2eccec1cfc
José Cabrero-Holgueras <jose.cabrero@nillion.com>
2024-06-25 16:13:47 +0000

    fix: linting

--- nillion_python_helpers/payments.py
+++ nillion_python_helpers/payments.py
@@ -1,9 +1,9 @@
 import os
-import py_nillion_client as nillion
 
+import py_nillion_client as nillion
 from cosmpy.aerial.client import NetworkConfig
-from cosmpy.aerial.tx import Transaction
 from cosmpy.aerial.client.utils import prepare_and_broadcast_basic_transaction
+from cosmpy.aerial.tx import Transaction
 from cosmpy.crypto.address import Address
 
 
@@ -30,6 +30,7 @@ async def pay(
     )
     return nillion.PaymentReceipt(quote, submitted_tx.tx_hash)
 
+
 async def quote(
     client: nillion.NillionClient,
     operation: nillion.Operation,
@@ -40,6 +41,7 @@ async def quote(
     print("Getting quote for operation...")
     quote = await client.request_price_quote(cluster_id, operation)
     return quote
+
 
 async def pay_with_quote(
     client: nillion.NillionClient,
@@ -60,6 +62,7 @@ async def pay_with_quote(
         f"Submitting payment receipt {quote.cost.total} unil, tx hash {submitted_tx.tx_hash}"
     )
     return nillion.PaymentReceipt(quote, submitted_tx.tx_hash)
+
 
 def create_payments_config(chain_id, payments_endpoint):
 
