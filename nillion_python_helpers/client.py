36c0e8d9da2e18fcfb1b77688172e2b6b0ddb062
oceans404 <steph.orpilla@nillion.com>
2024-08-19 12:22:51 -0700

    linting fix

--- nillion_python_helpers/client.py
+++ nillion_python_helpers/client.py
@@ -1,7 +1,5 @@
 import os
-
 import py_nillion_client as nillion
-
 
 def create_nillion_client(userkey, nodekey, bootnodes=None):
     """
@@ -20,5 +18,8 @@ def create_nillion_client(userkey, nodekey, bootnodes=None):
         bootnodes = [os.getenv("NILLION_BOOTNODE_MULTIADDRESS")]
 
     return nillion.NillionClient(
-        nodekey, bootnodes, nillion.ConnectionMode.relay(), userkey
+        nodekey,
+        bootnodes,
+        nillion.ConnectionMode.relay(),
+        userkey,
     )
